01c03528d7e2a55c910a3a2e3a3123f323e5fc79
JosephSP <suarez.losif@gmail.com>
2024-07-07 03:18:49 -0400

    Modifica APP.js, agregado funcion encriptar y copiar

--- app.js
+++ app.js
@@ -1,30 +1,99 @@
 function encriptar(){
     console.log("encriptar")
     const txtencriptar = document.getElementById('IDIngresoTexto').value;
-    let regex = /^[a-z]+$/;
+    let regex = /^(\s*[a-z]\s*)+$/;
     if (txtencriptar == "" || !regex.test(txtencriptar)){
+        document.getElementsByClassName('resultadoConRespuesta')[0].style.display = "none";
+        document.getElementsByClassName('ResultadoSinRespuesta')[0].style.display = "block";
         return;
     }
 
     regex = /^[^eiaou]+$/;
-    
-    for (letra in txtencriptar){
-        if (regex.test(txtencriptar[letra])){
-            console.log(txtencriptar[letra])
-
+    let txtencriptado = ""
+    let letra = ''
+    for (Nletra in txtencriptar){
+        letra = txtencriptar[Nletra]
+        // console.log(letra)
+        if (regex.test(letra)){
+            txtencriptado += letra;
+            continue;
+        }
+        switch (letra){
+            case 'e':
+                txtencriptado += "enter";
+                break;
+            case 'i':
+                txtencriptado += "imes";
+                break;
+            case 'a':
+                txtencriptado += "ai";
+                break;
+            case 'o':
+                txtencriptado += "ober";
+                break;
+            case 'u':
+                txtencriptado += "ufat";
+                break;
         }
     }
-
-    console.log(txtencriptar);
+    document.getElementsByClassName('resultadoConRespuesta')[0].style.display = "block";
+    document.getElementsByClassName('resultadoSinRespuesta')[0].style.display = "none";
+    document.getElementById('IDtxtResultado').value = txtencriptado
+    // console.log(txtencriptado);
 
 }
 
 function desencriptar(){
     console.log("desencriptar");
-    
+    const txtdesencriptar = document.getElementById('IDIngresoTexto').value;
+    let regex = /^(\s*[a-z]\s*)+$/;
+    if (txtdesencriptar == "" || !regex.test(txtdesencriptar)){
+        document.getElementsByClassName('resultadoConRespuesta')[0].style.display = "none";
+        document.getElementsByClassName('ResultadoSinRespuesta')[0].style.display = "block";
+        return;
+    }
+
+    regex = /^[^eiaou]+$/;
+    let txtdesencriptado = "";
+    let letra = '';
+    for (Nletra in txtdesencriptar){
+        letra = txtdesencriptar[Nletra];
+        // console.log(letra);
+        if (regex.test(letra)){
+            txtdesencriptado += letra;
+            continue;
+        }
+        switch (letra){
+            case 'e':
+                txtdesencriptado += "enter";
+                break;
+            case 'i':
+                txtdesencriptado += "imes";
+                break;
+            case 'a':
+                txtdesencriptado += "ai";
+                break;
+            case 'o':
+                txtdesencriptado += "ober";
+                break;
+            case 'u':
+                txtdesencriptado += "ufat";
+                break;
+        }
+    }
+    document.getElementsByClassName('resultadoConRespuesta')[0].style.display = "block";
+    document.getElementsByClassName('resultadoSinRespuesta')[0].style.display = "none";
+    document.getElementById('IDtxtResultado').value = txtdesencriptado;
 }
 
 function copiarResultado(){
     console.log("copiarResultado");
-    
+    var txtcopia = document.getElementById("IDtxtResultado");
+
+    // Select the text field
+    txtcopia.select();
+    txtcopia.setSelectionRange(0, 99999); // For mobile devices
+  
+     // Copy the text inside the text field
+    navigator.clipboard.writeText(txtcopia.value);
 }
